86864cdb6e55a3aa8bc4a1c1b14f5c0cb8f00961
Anthony <github@utopios.net>
2021-09-24 12:35:32 +0200

    Ajout correction exo 11

---
+++ Javascript/2-Exercices/Exo11-IfElse-ConsonneVoyelle/main.js
@@ -0,0 +1,40 @@
+/**
+ * Exercice11 - IfElse - Consonne...? Voyelle...?
+ */
+
+// Création de la constante result permettant de recupérer l'élément HTML class="result"
+const result = document.querySelector('.result');
+
+// Récupération et stockage des saisie utilisateur
+var userLetter = prompt("Veuillez saisir une lettre");
+userLetter = userLetter.toUpperCase()
+
+// // Etablissement Structure If() pour vérifier la saisie utilisateur
+// if (userLetter == "A" ||
+//     userLetter == "E" ||
+//     userLetter == "I" ||
+//     userLetter == "O" ||
+//     userLetter == "U" ||
+//     userLetter == "Y") {
+//     resultat = `La lettre ${userLetter} est une voyelle`;
+//     alert(resultat);
+// } else {
+//     resultat = `La lettre ${userLetter} est une consonne`;
+//     alert(resultat);
+// }
+
+// Etablissement Structure If() pour vérifier la saisie utilisateur (Avec Liste Voyelle)
+const voyelle = ["A", "E", "I", "O", "U", "Y"];
+if (voyelle.includes(userLetter)) {
+    resultat = `La lettre ${userLetter} est une voyelle`;
+    alert(resultat);
+} else {
+    resultat = `La lettre ${userLetter} est une consonne`;
+    alert(resultat);
+}
+
+// // En ternaire
+// resultat = voyelle.includes(userLetter) ? `La lettre ${userLetter} est une voyelle` : `La lettre ${userLetter} est une consonne`;
+
+// Injection du résultat dans l'element HTML .result
+result.innerHTML = `${resultat}`;
