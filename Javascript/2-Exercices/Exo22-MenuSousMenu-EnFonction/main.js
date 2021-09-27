f92e7cf46b8ac06fff8559d98705dc2a5b5e8a54
ihab abadi <github@utopios.net>
2021-09-27 15:39:49 +0200

    correction menusousMenu partie 2

--- Javascript/2-Exercices/Exo22-MenuSousMenu-EnFonction/main.js
+++ Javascript/2-Exercices/Exo22-MenuSousMenu-EnFonction/main.js
@@ -1,15 +1,36 @@
 const result = document.querySelector("#result")
-const taille = 4
+const chapitre = document.querySelector("#chapitre")
+const partie = document.querySelector("#partie")
+const creation = document.querySelector("#creation")
+//const taille = 4
 
 //Récupérer les valeurs de chaque champs
 //Ajouter un evenement click sur le bouton
 //Générer le menu
-for(let i=1; i <= taille; i++) {
-    let bloc = `<h3 class="row"><span class="col">Chapitre ${i}</span></h3>`
-    bloc += "<ul class='row list-group'>"
-    for(let j=1; j <= taille; j++) {
-        bloc += `<li class='col-12 list-group-item'>Partie ${i}.${j}</li>`
+
+
+
+//Fonction qui génère le menu et sous menu en fonction des paramètres chapitre et partie
+const makeMenu = (chapitre, partie) => {
+    let bloc = ""
+    for (let i = 1; i <= chapitre; i++) {
+        bloc += `<h3 class="row"><span class="col">Chapitre ${i}</span></h3>`
+        bloc += "<ul class='row list-group'>"
+        for (let j = 1; j <= partie; j++) {
+            bloc += `<li class='col-12 list-group-item'>Partie ${i}.${j}</li>`
+        }
+        bloc += "</ul>"
     }
-    bloc += "</ul>"
-    result.innerHTML+= bloc
+    return bloc
 }
+
+const executionClick = () => {
+    const valChapitre = parseInt(chapitre.value)
+    const valPartie = parseInt(partie.value)
+
+    //Appelle la fonction makeMenu et on injecte le résultat dans l'html de la div result
+    result.innerHTML = makeMenu(valChapitre, valPartie)
+}
+
+//Event click sur le bouton
+creation.addEventListener('click', executionClick)
