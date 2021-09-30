3ba4e4d127d8e8d1de92cddc4c1691d39bc26007
ihab abadi <github@utopios.net>
2021-09-30 09:57:46 +0200

    Correction exo 17 update

--- Javascript/2-Exercices/BaseJS/Exo16-IfElse-PrimeLicenciement/main.js
+++ Javascript/2-Exercices/BaseJS/Exo16-IfElse-PrimeLicenciement/main.js
@@ -1,49 +1,39 @@
 /**
- * EXERCICE17 - IFELSE - Calcul impots
+ * EXERCICE16 - IFELSE - Taille Vêtements
  */
 
 // Création de la constante result permettant de recupérer l'élément HTML class="result"
 const result = document.querySelector('.result');
 
 // Déclaration de variables
-let revenus = 0,
-    nbAdulte = 0,
-    nbEnfant = 0,
-    nbParts = 0,
-    revenuImposable = 0,
-    montantImpot = 0;
-
+let indemnite = 0;
 
 // Récupération et stockage des saisie utilisateur
-revenus = parseInt(prompt("Veuillez saisir Le revenu fical de référence :"));
-nbAdulte = parseInt(prompt("Veuillez saisir le nombre d'adultes :"));
-nbEnfant = parseInt(prompt("Veuillez saisir le nombre d'enfants :"));
+let salaire = parseInt(prompt("Veuillez saisir le dernier :"));
+let anciennete = parseInt(prompt("Veuillez saisir l'ancienneté :"));
+let age = parseInt(prompt("Veuillez saisir l'age :"));
 
-// Calcul du nombre de part
-if (nbEnfant <= 2)
-    nbParts = nbAdulte + nbEnfant * 0.5;
-else
-    nbParts = nbAdulte + nbEnfant - 1;
+// Etablissement d'une structure conditionnelle afin de déterminer le montant de la prime
+if (anciennete >= 1 && anciennete <= 10) {
+    indemnite += anciennete * salaire / 2;
+}
+if (anciennete > 10) {
+    indemnite += 10 * salaire / 2;
+    indemnite += (anciennete - 10) * salaire;
+}
 
-// // Calcul du nombre part en sugar syntaxe
-// nbParts = nbEnfant <= 2 ? nbAdulte + nbEnfant * 0.5 : nbAdulte + nbEnfant - 1;
+if (age > 45) {
 
-// Calcul du montant des l'impôt
-revenuImposable = revenus / nbParts;
+    if (age < 50) {
+       indemnite+= 2 * salaire;
+    } else {
+       indemnite+= 5 * salaire;
+    }
 
-if (revenuImposable >= 10085 && revenuImposable <= 25710)
-    montantImpot = Math.round((revenuImposable - 10084) * 0.11);
-if (revenuImposable >= 25711 && revenuImposable <= 73516)
-    montantImpot = Math.round((revenuImposable - 25711) * 0.30 + (25711 - 10085) * 0.11);
-if (revenuImposable >= 73517 && revenuImposable <= 158122)
-    montantImpot = Math.round((revenuImposable - 73516) * 0.41 + (73516 - 25710) * 0.30 + (25711 - 10085) * 0.11);
-if (revenuImposable >= 158123)
-    montantImpot = Math.round((revenuImposable - 158122) * 0.45 + (158122 - 73516) * 0.41 + (73516 - 25710) * 0.30 + (25711 - 10085) * 0.11);
+    //indemnite += age < 50 ? 2 * salaire : 5 * salaire;
+}
 
-montantImpot *= nbParts;
-
-console.log(montantImpot);
 
 // Injection du résultat dans l'element HTML .result
-result.innerHTML = `<p>Le montant de l'impôt pour un revenu fiscal de <b>${revenus}€</b> avec  <b>${nbAdulte} alulte(s)</b>
-et <b>${nbEnfant} enfant(s)</b> s'éléve à <b>${montantImpot}€</b>. </p>`;
+result.innerHTML = `<p>Le montant de l'indemnité pour un salaire de <b>${salaire}€</b> et un age de <b>${age} ans</b>
+avec une ancienneté de <b>${anciennete}</b> s'éléve à <b>${indemnite}€</b>.</p>`;
