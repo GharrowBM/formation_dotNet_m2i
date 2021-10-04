33d66b99e1ded395ff8d3b3e527e7009168c83e3
ihab abadi <github@utopios.net>
2021-10-04 14:14:00 +0200

    suite cours express avec element static

--- nodejs/correction-ex1/index.js
+++ nodejs/correction-ex1/index.js
@@ -2,6 +2,16 @@ const express = require("express")
 const path = require("path")
 
 const webApp = express()
+//Graçe au middleware, on peut ajouter une redirection vers un dossier static
+
+webApp.use(express.static(path.join(`${__dirname}/static`)))
+
+//Créer une routes pour la page style.css
+
+// webApp.get('/style', (req, res) => {
+//     res.sendFile(path.join(`${__dirname}/css/style.css`))
+
+// })
 
 //route pour accéder à l'accueil
 webApp.get('/', (req,res) => {
@@ -19,6 +29,7 @@ webApp.get('/description', (req,res) => {
 webApp.get('/qui-sommes-nous', (req,res) => {
     res.sendFile(path.join(`${__dirname}/html/qui-sommes-nous.html`))
 })
+
 
 
 webApp.listen(80)
