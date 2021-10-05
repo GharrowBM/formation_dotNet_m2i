41a91792cc540ffbb2270b815f42ac1a165bff5f
ihab abadi <github@utopios.net>
2021-10-05 09:32:36 +0200

    suite ejs boucle for

--- nodejs/cours-express-ejs/index.js
+++ nodejs/cours-express-ejs/index.js
@@ -2,6 +2,11 @@ const express = require("express")
 const path = require("path")
 const app = express()
 
+const users = [
+    {firstName: 'ihab', lastName: 'abadi'},
+    {firstName: 'tata', lastName: 'toto'},
+]
+
 //Le moteur de rendu utilisé est ejs
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
@@ -13,5 +18,18 @@ app.get('/contact', (req, res) => {
    // res.sendFile(path.join(__dirname+"/html/contact.html"))
    res.render("html/contact")
 
+})
+
+app.get('/description', (req,res) => {
+    let description = "Ma description dynamique"
+    res.render("html/description", {
+       description: description 
+    })
+})
+
+app.get('/users', (req, res) => {
+    res.render("html/users", {
+        utilisateurs: users
+    })
 })
 app.listen(80)
