2d83fc688960d029575985b04f3ec29d70b5d6d5
GharrowBM <gharrow.games@gmail.com>
2021-10-14 13:35:47 +0200

    Todo kévin B

---
+++ React/Todo_lists/todo-kevin/src/components/Header.js
@@ -0,0 +1,13 @@
+import '../style/Header.css'
+
+function Header(){
+    return (
+        <div>
+            <div className="header">
+                <h2>React to do list</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ React/Todo_lists/todo-kevin/src/datas/CoursList.js
@@ -0,0 +1,17 @@
+export const coursList =[
+    {
+        id: 1,
+        text: "Apprendre react",
+        done:false
+    },
+    {
+        id: 2,
+        text: "Faire des exercice",
+        done:false
+    },
+    {
+        id: 3,
+        text: "Créer des projets personnels",
+        done:false
+    },
+]
