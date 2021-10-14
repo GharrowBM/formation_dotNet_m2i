3062c8ae5a9caf0c810c4ca509d4c8d6cb4de985
ihab abadi <github@utopios.net>
2021-10-14 11:23:21 +0200

    Correction ExoChristophe

--- React/exoChristophe/exo-react/src/components/Formulaire.js
+++ React/exoChristophe/exo-react/src/components/Formulaire.js
@@ -24,23 +24,27 @@ function Formulaire({ ToDoList, setToDoList }) {
         console.log("tmpTodos[1] : " + tmpTodos[1]['text']);
         console.log("tmpTodos[2] : " + tmpTodos[2]['text']);
         console.log("ToDoList.Length = " + ToDoList.length);
-        console.log("ToDoList[0].id = " + ToDoList.length[0].id);
+        console.log("ToDoList[0].id = " + ToDoList[0].id);
         
         let newTodo = {
             id: (ToDoList[ToDoList.length - 1] !== undefined) ? (ToDoList[ToDoList.length - 1].id + 1) : 1,
-            done: false,
-            text: todo
+            text: todo,
+            done: false            
         }
-        console.log("newTodo.id : " + tmpTodos.id);
-        console.log("newTodo.text : " + tmpTodos.text);
-        console.log("newTodo.done : " + tmpTodos.done);
+
+        console.log("newTodo.id : " + newTodo.id);
+        console.log("newTodo.text : " + newTodo.text);
+        console.log("newTodo.done : " + newTodo.done);
+        console.log("newTodo : " + newTodo);
         tmpTodos.push(newTodo)
+        console.log("ToDoList : "+ToDoList);
 
-        // console.log("NewTodo: "+newTodo);
-        // console.log(this.state.todos);
-        setToDoList(...ToDoList, tmpTodos)
+        console.table("tmpTodos: "+tmpTodos);
+        
+        setToDoList(tmpTodos)
+        console.log("ToDoList"+ToDoList);
 
-        //console.log(this.state.todos);
+        
         setTodo("")
     }
 
