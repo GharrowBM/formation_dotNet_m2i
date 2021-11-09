04f74665e217b52feed69e9e1acab39a8d201e2b
PetitVulcan <dipersio.a@gmail.com>
2021-11-09 12:24:08 +0100

    Correction exercice 7

---
+++ DotNET/FormationDotNet/ExercicesCSharp/Exercice7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercice7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Déclaration des variables
+            double premierCote;
+            double deuxiemeCote;
+            double sommeDesCarres;
+            double resultat;
+            #endregion
+
+
+            #region Affichage du titre de l'app
+            Console.Write("--- Calcul de la longeur de l'hypothénuse ---" + Environment.NewLine);
+            #endregion
+
+
+            #region Recupération des saisies utilisateur
+            Console.Write("Entrez la longeur du premier coté (en cm) : ");
+            premierCote = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Entrez la longeur du deuxième coté (en cm) : ");
+            deuxiemeCote = Convert.ToDouble(Console.ReadLine());
+            #endregion
+
+
+            #region Calcul de l'hypothénuse
+            sommeDesCarres = Math.Pow(premierCote, 2) + Math.Pow(deuxiemeCote, 2);
+            resultat = Math.Round(Math.Sqrt(sommeDesCarres), 2);
+            #endregion
+
+
+            #region Affichage des résultats
+            Console.WriteLine($"La longueur de l'hypothénuse est {resultat} cm\n");
+            #endregion
+        }
+    }
+}
