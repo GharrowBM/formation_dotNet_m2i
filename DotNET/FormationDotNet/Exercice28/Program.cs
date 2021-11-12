c7dffaeb24e5d0b4c976bc9d0ad51d284405d1a6
ihab abadi <github@utopios.net>
2021-11-12 12:15:21 +0100

    correction exercice 28

---
+++ DotNET/FormationDotNet/Exercice28/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercice28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombre = 0;
+            Random r = new Random();
+            int count = 0;
+            int nbMystere = r.Next(1, 51);
+            while (nombre != nbMystere)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Merci de saisir un nombre : ");
+                nombre = Convert.ToInt32(Console.ReadLine());
+                if(nombre > nbMystere)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Plus petit");
+                    count++;
+                }
+                else if(nombre < nbMystere)
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                    Console.WriteLine("Plus grand");
+                    count++;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Bravo ! en {count} essai");
+        }
+    }
+}
