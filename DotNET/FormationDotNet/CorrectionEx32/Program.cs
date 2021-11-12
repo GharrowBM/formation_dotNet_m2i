851ede3d0f1e59c1653d6c74c69b3e65279bfacb
ihab abadi <github@utopios.net>
2021-11-12 14:23:26 +0100

    correction exercice 32

---
+++ DotNET/FormationDotNet/CorrectionEx32/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CorrectionEx32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merci de saisir la taille du tableau : ");
+            int taille = Convert.ToInt32(Console.ReadLine());
+            int[] tabEntier = new int[taille];
+            for (int i = 0; i < taille; i++)
+            {
+                Console.Write($"Merci de saisir la valeur de la case {i+1} : ");
+                tabEntier[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("=====Le contenu du tableau====");
+            for(int i = 0;i < tabEntier.Length; i++)
+            {
+                Console.WriteLine(tabEntier[i]);
+            }
+        }
+    }
+}
