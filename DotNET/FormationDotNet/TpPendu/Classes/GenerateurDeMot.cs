0b05f018acab246cfa10c473b2d3a502ebd533dc
ihab abadi <github@utopios.net>
2021-11-15 16:50:52 +0100

    debut de correction pendu

---
+++ DotNET/FormationDotNet/TpPendu/Classes/GenerateurDeMot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpPendu.Classes
+{
+    internal class GenerateurDeMot
+    {
+        private Random random = new Random();
+        private string[] mots = new string[] { "google", "apple", "microsoft", "amazon", "facebook" };
+
+        public string Generer()
+        {
+            return mots[random.Next(mots.Length)];
+        }
+    }
+}
---
+++ DotNET/FormationDotNet/TpPendu/Classes/Pendu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpPendu.Classes
+{
+    internal class Pendu
+    {
+        private int nbEssai;
+        private string masque;
+        private string motAtrouve;
+
+        public int NbEssai { get => nbEssai;  }
+        public string Masque { get => masque;}
+        public string MotAtrouve { get => motAtrouve; }
+
+        public Pendu()
+        {
+            nbEssai = 10;
+        }
+
+        public bool TestChar(char c)
+        {
+            string tmpMasque = "";
+            bool test = false;
+            for(int i = 0; i < MotAtrouve.Length; i++)
+            {
+                if(MotAtrouve[i] == c)
+                {
+                    tmpMasque += c;
+                    test = true;
+                }
+                else
+                {
+                    tmpMasque += masque[i];
+                    nbEssai--;
+                }
+            }
+            masque = tmpMasque;
+            return test;
+        }
+
+        public bool testWin()
+        {
+            return NbEssai > 0 && Masque == MotAtrouve;
+        }
+
+        public void GenererMasque(string mot)
+        {
+            motAtrouve = mot;
+            masque = "";
+            foreach(char c in MotAtrouve)
+            {
+                masque += "*";
+            }
+        }
+    }
+}
