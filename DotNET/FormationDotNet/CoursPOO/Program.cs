6befb6cd5a93b5eb200420bf185787b0389418e8
ihab abadi <github@utopios.net>
2021-11-15 10:27:19 +0100

    cours poo objet

---
+++ DotNET/FormationDotNet/CoursPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿using CoursPOO;
+//using CoursPOO.AirBus;
+//using CoursPOO.Boeing;
+using System;
+namespace CoursPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Avion avion1 = new Avion();
+            avion1.nbPlaces = 189;
+            avion1.couleur = "rouge";
+            Console.WriteLine(avion1.nbPlaces);
+            Console.WriteLine(avion1.couleur);
+            Console.WriteLine(avion1.GetType());
+        }
+    }
+}
