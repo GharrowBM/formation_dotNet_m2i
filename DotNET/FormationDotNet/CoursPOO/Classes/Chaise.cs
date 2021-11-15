5c50d58b4ea95ecbf05539d0e238998f4e4c0346
ihab abadi <github@utopios.net>
2021-11-15 11:43:09 +0100

    correction exercice chaise

---
+++ DotNET/FormationDotNet/CoursPOO/Classes/Chaise.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursPOO.Classes
+{
+    internal class Chaise
+    {
+        public int nbPieds;
+        public string couleur;
+        public string materiel;
+
+        public Chaise()
+        {
+
+        }
+
+        public Chaise(int nbPieds, string couleur, string materiel)
+        {
+            this.nbPieds = nbPieds;
+            this.couleur = couleur;
+            this.materiel = materiel;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine($"Je suis une chaise, avec {nbPieds} pieds en {materiel} et de couleur {couleur}");
+        }
+    }
+}
--- DotNET/FormationDotNet/CoursPOO/Program.cs
+++ DotNET/FormationDotNet/CoursPOO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CoursPOO;
+using CoursPOO.Classes;
 //using CoursPOO.AirBus;
 //using CoursPOO.Boeing;
 using System;
@@ -8,14 +9,31 @@ namespace CoursPOO
     {
         static void Main(string[] args)
         {
-            Avion avion1 = new Avion("rouge", 189);
-            //avion1.nbPlaces = 189;
-            //avion1.couleur = "rouge";
-            Console.WriteLine(avion1.nbPlaces);
-            Console.WriteLine(avion1.couleur);
-            string result = avion1.Voler();
-            Console.WriteLine(result);
-            Console.WriteLine(avion1.GetType());
+            //Avion avion1 = new Avion("rouge", 189);
+            ////avion1.nbPlaces = 189;
+            ////avion1.couleur = "rouge";
+            //Console.WriteLine(avion1.nbPlaces);
+            //Console.WriteLine(avion1.couleur);
+            //string result = avion1.Voler();
+            //Console.WriteLine(result);
+            //Console.WriteLine(avion1.GetType());
+
+            #region exercice chaise
+            Chaise chaise1 = new Chaise(4, "rouge", "bois");
+            Chaise chaise2 = new Chaise(5, "blanche", "metal");
+            //chaise1.Afficher();
+            //chaise2.Afficher();
+            Chaise chaise3 = new Chaise();
+            chaise3.nbPieds = 6;
+            chaise3.couleur = "jaune";
+            chaise3.materiel = "plexiglass";
+            Chaise[] tabChaises = new Chaise[] {chaise1, chaise2, chaise3};
+            foreach(Chaise chaise in tabChaises)
+            {
+                chaise.Afficher();
+            }
+            //chaise3.Afficher();
+            #endregion  
         }
     }
 }
