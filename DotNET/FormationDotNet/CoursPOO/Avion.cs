c37f82640d4ba2e6f4882cef1bf4ec9c143077f9
ihab abadi <github@utopios.net>
2021-11-15 11:07:05 +0100

    cours  poo partie 1

--- DotNET/FormationDotNet/CoursPOO/Avion.cs
+++ DotNET/FormationDotNet/CoursPOO/Avion.cs
@@ -10,5 +10,24 @@ namespace CoursPOO
     {
         public string couleur;
         public int nbPlaces;
+
+        //Constructeur
+
+        public Avion()
+        {
+
+        }
+        public Avion(string c, int n)
+        {
+            //Action a executer pendant la construction de votre objet
+            couleur = c;
+            nbPlaces = n;
+
+        }
+        public string Voler()
+        {
+            //Console.WriteLine($"L'avion de couleur {couleur} et avec un nombre de places {nbPlaces} a volé");
+            return $"L'avion de couleur {couleur} et avec un nombre de places {nbPlaces} a volé";
+        }
     }
 }
--- DotNET/FormationDotNet/CoursPOO/Program.cs
+++ DotNET/FormationDotNet/CoursPOO/Program.cs
@@ -8,11 +8,13 @@ namespace CoursPOO
     {
         static void Main(string[] args)
         {
-            Avion avion1 = new Avion();
-            avion1.nbPlaces = 189;
-            avion1.couleur = "rouge";
+            Avion avion1 = new Avion("rouge", 189);
+            //avion1.nbPlaces = 189;
+            //avion1.couleur = "rouge";
             Console.WriteLine(avion1.nbPlaces);
             Console.WriteLine(avion1.couleur);
+            string result = avion1.Voler();
+            Console.WriteLine(result);
             Console.WriteLine(avion1.GetType());
         }
     }
