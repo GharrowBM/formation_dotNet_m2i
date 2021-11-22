4af5735d3f23da6ef83c15855241e9705371dbcd
ihab abadi <github@utopios.net>
2021-11-22 11:11:47 +0100

    structure IHM caisse enregistreuse

--- DotNET/FormationDotNet/CaisseEnregistreuse/CaisseEnregistreuse/Classes/IHM.cs
+++ DotNET/FormationDotNet/CaisseEnregistreuse/CaisseEnregistreuse/Classes/IHM.cs
@@ -3,8 +3,54 @@ namespace CaisseEnregistreuse.Classes
 {
     public class IHM
     {
+        private Caisse caisse;
+        private Vente vente;
         public IHM()
         {
+            caisse = new Caisse();
+        }
+
+        public void Demarrer()
+        {
+
+        }
+
+        private void MenuPrincipal()
+        {
+            Console.WriteLine("1- Ajouter un produit");
+            Console.WriteLine("2- Faire une vente");
+            Console.WriteLine("0- Quitter");
+        }
+
+        private void MenuVente()
+        {
+            Console.WriteLine("1- Ajouter un produit à la vente");
+            Console.WriteLine("2- Paiement CB");
+            Console.WriteLine("3- Paiement Espece");
+        }
+
+        private void ActionAjouterProduit()
+        {
+
+        }
+
+        private void ActionFaireUneVente()
+        {
+
+        }
+
+        private void ActionAjoutProduitVente()
+        {
+
+        }
+
+        private void ActionPaiementCB()
+        {
+
+        }
+        private void ActionPaiementEspece()
+        {
+
         }
     }
 }
