3284d6ddd47c0606256d2f6f1e88f333091c5464
ihab abadi <github@utopios.net>
2021-11-23 12:19:02 +0100

    cours sql

---
+++ DotNET/FormationDotNet/CoursSQL.sql
@@ -0,0 +1,11 @@
+﻿-- LDD Langage de  définition de données
+--Exemple de commande CREATE, ALTER, DROP
+--Création de table
+CREATE TABLE  personne (
+	id int NOT NULL IDENTITY(1,1),
+	nom varchar(150),
+	prenom varchar(150)
+);
+
+--Supprimer une table
+DROP TABLE personne;
