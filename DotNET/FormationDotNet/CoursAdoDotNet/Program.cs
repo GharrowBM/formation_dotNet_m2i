81727d7145334c2c4e3d02ab5138baf357b6418b
ihab abadi <github@utopios.net>
2021-11-24 12:06:11 +0100

    cours ado.net

---
+++ DotNET/FormationDotNet/CoursAdoDotNet/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace CoursAdoDotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Objets pour se connecter à la base de données
+            string connectionString = @"Data Source=(LocalDb)\coursDotNet;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            //Console.WriteLine(connection.State);
+            //connection.Open();
+            //Console.WriteLine(connection.State);
+            //connection.Close();
+            //Console.WriteLine(connection.State);
+            //Pour executer des requêtes, on aura besoin des objets de types SqlCommand;
+            string titre = "produit en c# 2";
+            decimal prix = 1200;
+            string request = "INSERT INTO produit(titre, prix) " +
+                " OUTPUT INSERTED.id values (@t, @p)";
+            SqlCommand command = new SqlCommand(request, connection);
+            //Ajouter des paramètres de requête
+            command.Parameters.Add(new SqlParameter("@t", titre));
+            command.Parameters.Add(new SqlParameter("@p", prix));
+            connection.Open();
+            //1ere façon d'executer une commande => si aucun retour attendu.
+            //int nbRow = command.ExecuteNonQuery();
+            //2eme façon d'executer une commande => une seul et unique resultat attendu.
+            int id = (int)command.ExecuteScalar();
+            command.Dispose();
+            connection.Close();
+            Console.WriteLine(id);
+        }
+    }
+}
