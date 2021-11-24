d820abb7e8e8d608568e6d9babb7428901fa2396
ihab abadi <github@utopios.net>
2021-11-24 09:36:29 +0100

    cours requête imbriquée

--- DotNET/FormationDotNet/coursJointureSQL.sql
+++ DotNET/FormationDotNet/coursJointureSQL.sql
@@ -22,3 +22,27 @@ on p.id = a.personne_id;
 --right join
 SELECT p.id as personne_id, a.id as adresse_id, a.ville FROM personne as p right join adresse as a 
 on p.id = a.personne_id;
+
+CREATE TABLE produit(
+	id int PRIMARY KEY IDENTITY(1,1),
+	titre varchar(100) not null,
+	prix decimal not null
+);
+
+INSERT INTO produit(titre, prix) values
+('product 1', 100),
+('product 2', 200),
+('product 3', 300);
+
+
+--Utilisation des fonctions
+SELECT count(*) as nombre from produit where prix > 20;
+SELECT SUM(prix) as totalPrix from produit;
+SELECT AVG(prix) as moyenne from produit;
+
+--utilisation des requêtes imbriquées.
+SELECT AVG(prix) as moyenne from
+(SELECT prix from produit where titre like '%produit%') as produitTmp;
+
+SELECT SUM(prix) from produit where 
+id IN (SELECT id from produit where titre like '%product%');
