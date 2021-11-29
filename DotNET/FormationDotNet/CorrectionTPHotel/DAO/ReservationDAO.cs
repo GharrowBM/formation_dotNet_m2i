66101995e14ef3093eb82a7c88571f267c68f41d
ihab abadi <github@utopios.net>
2021-11-29 10:31:37 +0100

    correction tp hotel

--- DotNET/FormationDotNet/CorrectionTPHotel/DAO/ReservationDAO.cs
+++ DotNET/FormationDotNet/CorrectionTPHotel/DAO/ReservationDAO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CorrectionTPHotel.Classes;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,48 @@ using System.Threading.Tasks;
 
 namespace CorrectionTPHotel.DAO
 {
-    internal class ReservationDAO
+    internal class ReservationDAO : BaseDAO
     {
+        public Reservation Save(Reservation reservation)
+        {
+            request = "INSERT INTO reservation (client_id, statut) " +
+                "OUTPUT INSERTED.ID values(@client_id, @statut)";
+            connection = Connection;
+            SqlTransaction _transaction = null;
+            connection.Open();
+            try
+            {
+                _transaction = connection.BeginTransaction();
+                command = new SqlCommand(request, connection);
+                command.Transaction = _transaction;
+                command.Parameters.Add(new SqlParameter("@client_id", reservation.Client.Id));
+                command.Parameters.Add(new SqlParameter("@statut", reservation.Statut));
+                reservation.Id = (int)command.ExecuteScalar();
+                command.Dispose();
+                reservation.ChambreList.ForEach(chambre => SaveReservationChambre(reservation.Id, chambre.Id, connection, _transaction));                            
+                _transaction.Commit();
+            }
+            catch(Exception ex)
+            {
+                _transaction.Rollback();
+            }finally
+            {
+                connection.Close();          
+            }
+            return reservation;
+        }
+
+
+        public void SaveReservationChambre(int reservationId, int chambreId, SqlConnection _connection, SqlTransaction _transaction)
+        {
+            request = "INSERT INTO reservation_chambre (chambre_id, reservation_id) " +
+                "values (@chambre_id, @reservation_id)";
+            command = new SqlCommand(request, _connection);
+            command.Transaction = _transaction;
+            command.Parameters.Add(new SqlParameter("chambre_id", chambreId));
+            command.Parameters.Add(new SqlParameter("reservation_id", reservationId));
+            command.ExecuteNonQuery();
+            command.Dispose();
+        }
     }
 }
