ddbaa72959dc8145cb7f745a114383e35261a61b
ihab abadi <github@utopios.net>
2021-11-30 14:49:51 +0100

    suite cours wpf

---
+++ DotNET/FormationDotNet/CoursWPF/Classes/Voiture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursWPF.Classes
+{
+    public class Voiture
+    {
+        private string marque;
+        private string model;
+
+        public string Marque { get => marque; set => marque = value; }
+        public string Model { get => model; set => model = value; }
+
+        public override string ToString()
+        {
+            return $"Model : {Model}, Marque : {Marque}";
+        }
+    }
+}
---
+++ DotNET/FormationDotNet/CoursWPF/VoitureWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using CoursWPF.Classes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoursWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour Voiture.xaml
+    /// </summary>
+    public partial class VoitureWindow : Window
+    {
+        Voiture voiture;
+        ObservableCollection<Voiture> voitureList;
+        public VoitureWindow()
+        {
+            InitializeComponent();
+            voitureList = new ObservableCollection<Voiture>();
+            maListeVoiture.ItemsSource = voitureList;
+        }
+
+        public void ValidClick(object sender, RoutedEventArgs eventArgs)
+        {
+            MessageBox.Show(marque.Text + " " + model.Text);
+            voiture = new Voiture()
+            {
+                Marque = marque.Text,
+                Model = model.Text,
+            };
+
+            voitureList.Add(voiture);
+            marque.Text = "";
+            model.Text = "";
+
+        }
+    }
+}
