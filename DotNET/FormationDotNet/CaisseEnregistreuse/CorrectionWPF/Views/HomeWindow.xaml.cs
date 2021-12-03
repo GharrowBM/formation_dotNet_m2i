300276660aa2de7559e653198982e26834098cec
ihab abadi <github@utopios.net>
2021-12-03 11:10:08 +0100

    correction caisse enregistreuse wpf

---
+++ DotNET/FormationDotNet/CaisseEnregistreuse/CorrectionWPF/Views/HomeWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using CorrectionWPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CorrectionWPF.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour HomeWindow.xaml
+    /// </summary>
+    public partial class HomeWindow : Window
+    {
+        public HomeWindow()
+        {
+            InitializeComponent();
+            DataContext = new HomeViewModel();
+        }
+    }
+}
