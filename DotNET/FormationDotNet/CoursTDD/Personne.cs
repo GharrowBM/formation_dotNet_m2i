4caf50097b2141e242ae88548b2fc6c91dcbb565
ihab abadi <github@utopios.net>
2021-12-08 10:10:33 +0100

    suite test unitaire tdd

---
+++ DotNET/FormationDotNet/CoursTDD/Personne.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursTDD
+{
+    public class Personne
+    {
+        private string nom;
+        private string prenom;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        
+        public static List<Personne> personnes = new List<Personne>()
+        {
+            new Personne("toto", "tata"),
+            new Personne("minet", "titi"),
+        };
+
+        public Personne(string nom, string prenom)
+        {
+            Nom = nom;
+            Prenom = prenom;
+        }
+
+        public static Personne RechercherPersonne(string nom)
+        {
+            Personne personne = default(Personne);
+            foreach(Personne person in personnes)
+            {
+                if(person.Nom == nom)
+                {
+                    personne = person;
+                    break;
+                }
+            }
+            return personne;
+        }
+    }
+}
---
+++ DotNET/FormationDotNet/CousTestTDD/PersonneTest.cs
@@ -0,0 +1,28 @@
+﻿using CoursTDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CousTestTDD
+{
+    [TestClass]
+    public class PersonneTest
+    {
+        [TestMethod]
+        public void RechercherPersonne_Nom_Toto_Personne()
+        {
+            Personne p = Personne.RechercherPersonne("toto");
+            Assert.IsInstanceOfType(p, typeof(Personne));
+        }
+
+        [TestMethod]
+        public void RechercherPersonne_Nom_Tata_Default()
+        {
+            Personne p = Personne.RechercherPersonne("tata");
+            Assert.AreEqual(default(Personne), p);
+        }
+    }
+}
