bc29072ef2071edf4eee46e90338cc93a7fbaf7a
ihab abadi <github@utopios.net>
2021-12-08 09:52:23 +0100

    suite test unitaire tdd

--- DotNET/FormationDotNet/CousTestTDD/CalculatriceTest.cs
+++ DotNET/FormationDotNet/CousTestTDD/CalculatriceTest.cs
@@ -11,12 +11,29 @@ namespace CousTestTDD
     [TestClass]
     public class CalculatriceTest
     {
+        //A => Arrange one's
+        private Calculatrice c = new Calculatrice();
+        
         [TestMethod]
         public void AdditionTest()
         {
-            Calculatrice c = new Calculatrice();
-            double result = c.Addition(10, 30);
-            Assert.AreEqual(40, result);
+            //La régle du AAA
+            //A => Arrange
+            //Calculatrice c = new Calculatrice();
+            
+            //A => Act
+            double result = c.Addition(1, 0);
+            
+            //A => Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void AdditionTest2()
+        {
+            //Calculatrice c = new Calculatrice();
+            double result = c.Addition(1, 1);
+            Assert.AreEqual(2, result);
         }
     }
 }
