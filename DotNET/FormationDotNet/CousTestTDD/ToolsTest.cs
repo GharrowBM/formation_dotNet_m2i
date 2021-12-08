e8bf1453775c8be0c2c56bb7f84a6b6261afcea8
ihab abadi <github@utopios.net>
2021-12-08 15:08:08 +0100

    suite dev TDD

---
+++ DotNET/FormationDotNet/CousTestTDD/ToolsTest.cs
@@ -0,0 +1,40 @@
+﻿using DevEnTDD.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CousTestTDD
+{
+    [TestClass]
+    public class ToolsTest
+    {
+        [TestMethod]
+        public void TestSuffle_Instance()
+        {
+            Tools<int> t =new Tools<int>();
+            int[] tab = t.Shuffle(new int[] { 1, 2, 3, 4 });
+            Assert.AreEqual(4, tab.Length);
+        }
+
+        [TestMethod]
+        public void TestSuffle_Instance_Default_Value()
+        {
+            Tools<int> t = new Tools<int>();
+            int[] tab1 = new int[] { 1, 2, 3, 4 };
+            int[] tab2 = t.Shuffle(tab1);
+            Assert.AreNotEqual(tab2[3], default(int));
+        }
+
+        [TestMethod]
+        public void TestSuffle_Suffle()
+        {
+            Tools<int> t = new Tools<int>();
+            int[] tab1 = new int[] { 1, 2, 3, 4 };            
+            Assert.AreNotEqual(1, tab1[0]);
+        }
+    }
+
+}
---
+++ DotNET/FormationDotNet/DevEnTDD/Classes/Tools.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevEnTDD.Classes
+{
+    public class Tools<T>
+    {
+        private Random random = new Random();
+        public T[] Shuffle(T[] tab)
+        {
+            
+           T[] t = new T[tab.Length];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                int indexTmp = random.Next(0, tab.Length);
+                T tTmp = tab[indexTmp];
+                tab[indexTmp] = tab[i];
+                tab[i] = tTmp;
+                t[i] = tab[i];
+            }
+            return t;
+        }
+    }
+}
