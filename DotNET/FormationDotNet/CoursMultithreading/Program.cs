5192601fdcaf502c6c0f45726ddf776a751ca83c
ihab abadi <github@utopios.net>
2021-12-14 14:20:12 +0100

    suite cours thread

--- DotNET/FormationDotNet/CoursMultithreading/Program.cs
+++ DotNET/FormationDotNet/CoursMultithreading/Program.cs
@@ -6,29 +6,71 @@ namespace CoursMultithreading
 {
     internal class Program
     {
+        static string resultat = "";
+
+        static object _lock = new object();
+
+        static Mutex m1 = new Mutex();
         static void Main(string[] args)
         {
             //Thread ta = new Thread(() => Afficher("A"));
             //ta.Start();
             //Thread tb = new Thread(() => Afficher("B"));
             //tb.Start();
+            //Thread tc = new Thread(() => Afficher("C"));
+            //tc.Start();
+            //ThreadPool.SetMinThreads(2,3);
 
-            ThreadPool.SetMinThreads(2,3);
+            //ThreadPool.QueueUserWorkItem((obj) => Afficher("A"));
+            //ThreadPool.QueueUserWorkItem((obj) => Afficher("B"));
 
-            ThreadPool.QueueUserWorkItem((obj) => Afficher("A"));
-            ThreadPool.QueueUserWorkItem((obj) => Afficher("B"));
+            //Thread.Sleep(10000);
 
-            Thread.Sleep(10000);
+            //La synchronisation des threads peut se faire à l'aide de la fonction lock
 
+            //La synchronisation des threads peut se faire également par des mutexs
+
+            //La synchronisation des threads peut se faire également par des semaphores
+            Random rand = new Random();
+            SemaphoreSlim semaphore = new SemaphoreSlim(3);
+            int i = 1;
+            while(i <= 10)
+            {
+
+                Thread t = new Thread(
+                    () =>
+                    {
+                        int compteur = i;
+                        i++;
+                        semaphore.Wait();
+                        Console.WriteLine($"Le thread numéro {compteur} va commencer");
+                        Console.WriteLine($"Le thread numéro {compteur} est en cours d'execution ");
+                        Thread.Sleep(rand.Next(3000, 5000));
+                        Console.WriteLine($"Le thread numéro {compteur} terminé");
+                        semaphore.Release();
+                    }
+                    );
+                t.Start();
+            }
         }
 
         static void Afficher(string chaine)
         {
-            for(int i = 1; i <= 10000; i++)
+            //lock (_lock)
+            //{
+            //    for (int i = 1; i <= 10; i++)
+            //    {
+            //        resultat += chaine;
+            //        Console.WriteLine(resultat);
+            //    }
+            //}
+            m1.WaitOne();
+            for (int i = 1; i <= 10; i++)
             {
-                Debug.WriteLine(chaine);
-                Console.Write(chaine);
+                resultat += chaine;
+                Console.WriteLine(resultat);
             }
+            m1.ReleaseMutex();
         }
     }
 }
