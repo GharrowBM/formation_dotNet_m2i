bcd86ac78617d7ea0a3fda9dc8452bf688bfb3c8
ihab abadi <github@utopios.net>
2021-12-17 11:28:44 +0100

    correction dockerfile siret

--- React/Exercices/TP-SiretAPI/tp-siret-api/src/actions/Entreprise.js
+++ React/Exercices/TP-SiretAPI/tp-siret-api/src/actions/Entreprise.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export const getSiretData = (siret) => {
     return (dispatch) => {
         dispatch({type:'START_FETCHING_DATA'})
-        axios.get("https://api.insee.fr/entreprises/sirene/V3/siret/"+siret, {headers : {'Authorization' : 'Bearer 9d5da7dd-0c5d-336a-ae5d-69218ceb699d'}})
+        axios.get("https://api.insee.fr/entreprises/sirene/V3/siret/"+siret, {headers : {'Authorization' : 'Bearer '+ process.env.REACT_APP_API_KEY}})
         .then(res => {
             if(res.data.header.statut === 200) {
                 dispatch({type:'END_FETCHING_DATA', data: res.data.etablissement})
