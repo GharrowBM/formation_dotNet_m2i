10a8166ce2c4513ba0a0fadeeb9dc21e00c2bf67
ihab abadi <github@utopios.net>
2021-12-17 11:55:53 +0100

    suite cours docker volume

--- nodejs/correction-panier-nodejes-express/index.js
+++ nodejs/correction-panier-nodejes-express/index.js
@@ -2,17 +2,13 @@ const express = require("express")
 
 const app = express()
 const bodyParser = require("body-parser")
-//stock nos produits dans un tableau js
-let products = [
-    {name: 'produit 1', description: 'description produit 1', price: 10, category: 'cat 1', id: 1},
-    {name: 'produit 2', description: 'description produit 2', price: 20, category: 'cat 1',  id: 2},
-    {name: 'produit 3', description: 'description produit 3', price: 30, category: 'cat 2'},
-    {name: 'produit 4', description: 'description produit 4', price: 40, category: 'cat 2'},
-    {name: 'produit 5', description: 'description produit 5', price: 50, category: 'cat 1'},
-    {name: 'produit 6', description: 'description produit 6', price: 60, category: 'cat 3'},
-    {name: 'produit 7', description: 'description produit 7', price: 70, category: 'cat 3'},
-]
 
+const fs = require("fs")
+//stock nos produits dans un tableau js
+let products = JSON.parse(fs.readFileSync("produits.json"))
+const update = () => {
+    fs.writeFileSync('produits.json', JSON.stringify(products))
+}
 //Dossier pour les ressources statiques (css, javascript)
 app.use(express.static(__dirname+"/assets"))
 
@@ -55,6 +51,7 @@ app.get('/deleteproduct/:id', (req, res) => {
     const id = req.params.id
     if(products.length > id) {
         products.splice(id,1)
+        update()
         res.redirect("/")
     }
 })
@@ -93,6 +90,7 @@ app.post('/submitProduct', (req, res) => {
             products[id] = {name:req.body.name, price: req.body.price, category: req.body.category, description: req.body.description}
         }
     }
+    update()
     res.redirect('/')
 })
 
