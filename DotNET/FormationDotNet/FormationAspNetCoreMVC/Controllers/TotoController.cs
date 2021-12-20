ba3bf438e693dc1ca8d49630ed041a05dcc093fe
ihab abadi <github@utopios.net>
2021-12-20 10:12:57 +0100

    cours aspNet core

---
+++ DotNET/FormationDotNet/FormationAspNetCoreMVC/Controllers/TotoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormationAspNetCoreMVC.Controllers
+{
+    public class TotoController : Controller
+    {
+        public IActionResult First()
+        {
+            return new ContentResult() { Content = "<h1>Hello From our first action</h1>", ContentType="text/html" };
+        }
+    }
+}
