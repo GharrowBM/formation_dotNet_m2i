f92a27b035da7963872049487d42b90c80a33a1e
ihab abadi <github@utopios.net>
2021-12-22 10:20:21 +0100

    correction compte bancaire mvc partie 1

---
+++ DotNET/FormationDotNet/CompteBancaireMVC/Controllers/CompteController.cs
@@ -0,0 +1,45 @@
+﻿using CompteBancaireVersion1.Classes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompteBancaireMVC.Controllers
+{
+    public class CompteController : Controller
+    {
+        private Banque banque;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search(string search)
+        {
+            Compte compte = null;
+            if(int.TryParse(search, out int id))
+            {
+                banque = new Banque();
+                compte = banque.RechercherCompte(id);
+                if(compte == null)
+                {
+                    ViewBag.ClassAlert = "alert-danger";
+                    ViewBag.Message = "Aucun compte avec ce numéro";
+                }
+            }
+            else
+            {
+                ViewBag.ClassAlert = "alert-danger";
+                ViewBag.Message = "Merci de saisir un numéro";
+            }
+            return View("Index", compte);
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitForm()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
