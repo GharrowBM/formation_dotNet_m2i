2299f59157f48179ca44e8f4d760d002ac2d1d19
ihab abadi <github@utopios.net>
2021-12-22 12:04:43 +0100

    suite correction compte bancaire mvc

---
+++ DotNET/FormationDotNet/CompteBancaireMVC/Controllers/OperationController.cs
@@ -0,0 +1,37 @@
+﻿using CompteBancaireVersion1.Classes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompteBancaireMVC.Controllers
+{
+    public class OperationController : Controller
+    {
+        Banque banque;
+        public IActionResult Form(int id, string type)
+        {
+            ViewBag.Type = type;
+            ViewBag.Id = id;
+            return View();
+        }
+
+        public IActionResult SubmitForm(decimal montant, int id, string type)
+        {
+            banque = new Banque();
+            Compte compte = banque.RechercherCompte(id);
+            object result = null;
+            if(compte != null)
+            {   
+                if((type =="depot" && compte.Depot(new Operation(montant))) || (type =="retrait" && compte.Retrait(new Operation(montant * -1)))) {
+                    result = new { Message = "Opération effectuée", ClassAlert = "alert-success" };
+                }else
+                {
+                    result =  new { Message = "Erreur opération", ClassAlert = "alert-danger" };
+                }
+            }
+            else
+            {
+                result = new { Message = "Aucun compte avec cet id", ClassAlert = "alert-danger" };
+            }
+            return RedirectToAction("Index", "Compte", result);
+        }
+    }
+}
