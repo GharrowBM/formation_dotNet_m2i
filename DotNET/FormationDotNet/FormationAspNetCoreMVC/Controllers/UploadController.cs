58c1de295ca7c43ea14c1ca36fee267f9d5ca75d
ihab abadi <github@utopios.net>
2021-12-23 09:53:51 +0100

    cours asp.net core

---
+++ DotNET/FormationDotNet/FormationAspNetCoreMVC/Controllers/UploadController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace FormationAspNetCoreMVC.Controllers
+{
+    public class UploadController : Controller
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        public UploadController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitUpload(IFormFile image)
+        {
+            string wwwRootPath = _webHostEnvironment.WebRootPath;
+            string path = Path.Combine(wwwRootPath, "img",image.FileName);
+            Stream stream = System.IO.File.Create(path);
+            image.CopyTo(stream);
+            stream.Close();
+            string basePath = "img/" + image.FileName;
+            return RedirectToAction("Form");
+        }
+    }
+}
