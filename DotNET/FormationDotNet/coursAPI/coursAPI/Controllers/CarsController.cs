c7e55a3bc973f72c572e6d5ce8380dfe02d01863
ihab abadi <utopios@imac-de-ihab.home>
2022-01-03 10:01:40 +0100

    debut cours api rest

---
+++ DotNET/FormationDotNet/coursAPI/coursAPI/Controllers/CarsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coursAPI.Controllers
+{
+    [Route("voitures")]
+    public class CarsController : ControllerBase
+    {
+        public CarsController()
+        {
+        }
+
+        //[Route("get")]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string>() { "v1", "v2", "v3" };
+        }
+
+        [HttpGet("one")]
+        public string GetOne()
+        {
+            return "v12";
+        }
+    }
+}
