2994de2e62869962c946cbdb061d5584eb40e9df
ihab abadi <utopios@imac-de-ihab.home>
2022-01-04 14:56:45 +0100

    génération token

---
+++ DotNET/FormationDotNet/CoursJWT/CoursJWT/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoursJWT.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoursJWT.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        LoginService _loginService;
+        public LoginController(LoginService loginService)
+        {
+            _loginService = loginService;
+        }
+        [HttpPost]
+        public IActionResult Login([FromForm]string email, [FromForm] string password)
+        {
+            string token = _loginService.GenerateToken(email, password);
+            if(token == null)
+            {
+                return NotFound();
+            }
+            return Ok(token);
+        }
+    }
+}
---
+++ DotNET/FormationDotNet/CoursJWT/CoursJWT/Services/LoginService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CoursJWT.Services
+{
+    public class LoginService
+    {
+        public LoginService()
+        {
+        }
+
+        public string GenerateToken(string email, string password)
+        {
+            //Verification dans la base de données de l'email et mot de passe
+            if(email == "ihab@utopios.net" && password == "123456")
+            {
+                //données à stocker dans le token
+                List<Claim> claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Email, "ihab@utopios.net"),
+                    new Claim(ClaimTypes.Name, "ihab"),
+                    new Claim(ClaimTypes.Role, "admin")
+                };
+
+                //Objet pour signer le token
+                SigningCredentials signingCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Bonjour je suis la clé de cryptage")), SecurityAlgorithms.HmacSha256);
+
+
+                //Créer notre jwt
+                JwtSecurityToken jwt = new JwtSecurityToken(issuer: "m2i", audience: "m2i", claims: claims, signingCredentials: signingCredentials, expires: DateTime.Now.AddDays(2));
+                return new JwtSecurityTokenHandler().WriteToken(jwt);
+            }
+            return null;
+        }
+    }
+}
