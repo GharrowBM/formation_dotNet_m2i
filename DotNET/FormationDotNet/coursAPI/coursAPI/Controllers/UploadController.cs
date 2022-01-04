8e4c231e6214e3ee01b43c6dadc3224f937cf224
ihab abadi <utopios@imac-de-ihab.home>
2022-01-04 09:53:22 +0100

    suite cours api, possibilité d'upload

---
+++ DotNET/FormationDotNet/coursAPI/coursAPI/Controllers/UploadController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace coursAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class UploadController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromForm] IFormFile file, [FromForm] string name)
+        {
+
+            //Logique métier d'upload 
+            return Ok(new { file = file, name = name });
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
