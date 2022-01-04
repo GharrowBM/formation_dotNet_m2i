ccebba9e254f38612f92387c07e48ca339a3d425
ihab abadi <utopios@imac-de-ihab.home>
2022-01-04 15:36:12 +0100

    suite jwt cours

--- DotNET/FormationDotNet/CoursJWT/CoursJWT/Startup.cs
+++ DotNET/FormationDotNet/CoursJWT/CoursJWT/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using CoursJWT.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CoursJWT
 {
@@ -28,6 +32,27 @@ namespace CoursJWT
 
             services.AddControllers();
             services.AddTransient<LoginService>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Bonjour je suis la clé de cryptage")),
+                    ValidateIssuer = true,
+                    ValidIssuer = "m2i",
+                    ValidateAudience = true,
+                    ValidAudience = "m2i"
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("admin", police =>
+                {
+                    police.RequireClaim(ClaimTypes.Role, "admin");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,7 +64,7 @@ namespace CoursJWT
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
